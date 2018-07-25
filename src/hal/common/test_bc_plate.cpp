fba651de5099e6e7bd22316fafbae5657888e3a8
sdusxw <sxw@sdboon.com>
2018-07-25 20:11:51 +0800

    test bc plate

---
+++ src/hal/common/test_bc_plate.cpp
@@ -0,0 +1,31 @@
+#include "net_tcp.h"
+#include <iostream>
+
+int main()
+{
+    NetTcpServer tcp_server_5232;
+    tcp_server_5232.open_bind_listen(5232);
+    while(true)
+    {
+        char * msg = NULL;
+        int length;
+        if(http_server8090.get_message(msg, length))
+        {
+            printf("Got message with length:\t%d\n", length);
+            char filename[128];
+            sprintf(filename, "tcp_%d.bin", length);
+            FILE *fid;
+            fid = fopen(filename,"wb");
+            if(fid == NULL)
+            {
+                printf("写出文件出错");
+            }
+            
+            fwrite(msg, length, 1, fid);
+            
+            fclose(fid);
+        }
+ 
+        usleep(1000);
+    }
+}
